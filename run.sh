22af5d19e64ca18c6816e11db058bcdf25d61141
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-06-22 16:51:48 -0400

    change defaults

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 python train.py --data_dir=./data/bronte \
---rnn_size 2 \
---num_layers 3 \
---seq_length 2 \
---batch_size 10000 \
---num_epochs 2 \
+--rnn_size 128 \
+--num_layers 2 \
+--seq_length 50 \
+--batch_size 50 \
+--num_epochs 50 \
 --save_checkpoints ./checkpoints \
 --save_model /artifacts
